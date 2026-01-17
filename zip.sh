663b3cfde92bbc7ef93dd947eea0806c11771aba
stefano morpurgo <stefano.morpurgo@auticon.it>
2026-01-17 16:22:20 +0100

    Remove old XUL/XPCOM files and unnecessary artifacts
    Removed:
    - chrome/ directory (old XUL/XPCOM source code)
    - skin/ directory (old XUL CSS)
    - locale/ directory (old XUL locale files)
    - doc/ directory (documentation images)
    - chrome.manifest, install.rdf, zip.sh (old build files)
    - originalCode.js, cardbookAPI.js, .xpi build artifacts
    
    Updated:
    - manifest.json: Use icons from icons/ directory instead of chrome/content/
    
    All original code is preserved in git history for reference.

--- zip.sh
+++
@@ -1,3 +0,0 @@
-#cd duplicateContactsManager@ddvo.net
-zip -r ../duplicatecontactsmanager-2.1.1.xpi . -x \*~ .git/\* .git README.md zip.sh mk .gitignore doc/\*
-#chrome/content/duplicateContactsManager\* @
